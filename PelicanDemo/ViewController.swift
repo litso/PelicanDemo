8f10710a9f8c2aebada94aef75dc59511ba947cb
Robert Manson <robmanson@gmail.com>
2017-09-26 16:01:37 -0700

    Initial Commit

---
+++ PelicanDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PelicanDemo
+//
+//  Created by Robert Manson on 9/26/17.
+//  Copyright © 2017 Clutter. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ PelicanDemoTests/PelicanDemoTests.swift
@@ -0,0 +1,36 @@
+//
+//  PelicanDemoTests.swift
+//  PelicanDemoTests
+//
+//  Created by Robert Manson on 9/26/17.
+//  Copyright © 2017 Clutter. All rights reserved.
+//
+
+import XCTest
+@testable import PelicanDemo
+
+class PelicanDemoTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
